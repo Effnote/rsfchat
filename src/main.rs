16ce0f70ced9523120651b1fbe634e420d6f4beb
Effnote <effnote@gmail.com>
2017-12-01 18:22:30 +0000

    Move program flow into a Controller.

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,12 @@ extern crate cursive;
 extern crate fchat;
 extern crate futures;
 extern crate tokio_core;
-
-use fchat::{Server, Ticket};
-use fchat::message::{client, server};
-use futures::{Future, Stream, Sink};
-use futures::sync::mpsc::{channel, Receiver, Sender};
-
-use tokio_core::reactor::Core;
+extern crate tokio_timer;
 
 mod ui;
-
-fn connect(ticket: &Ticket, character: String, rx: Receiver<client::Message>, tx: Sender<server::Message>) {
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let chat = fchat::connect(Server::Debug, &handle)
-        .and_then(|(sink, stream)| {
-            (
-                fchat::identify(
-                    sink,
-                    ticket,
-                    character,
-                    "RSFChat".to_owned(),
-                    "0.0.1".to_owned(),
-                ),
-                Ok(stream),
-            )
-        })
-        .and_then(|(_sink, stream)| {
-            stream.forward(tx.sink_map_err(|_| fchat::Error::Channel))
-        });
-    core.run(chat).unwrap();
-}
+mod config;
+mod controller;
 
 fn main() {
-    ui::start();
+    controller::start().unwrap();
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,18 @@
 use cursive::{self, Cursive, views};
 use fchat::Ticket;
 
-fn login_dialog() -> views::Dialog {
+use futures::sync::oneshot;
+
+use std;
+use std::sync::{Arc, Mutex};
+use std::thread::spawn;
+use std::sync::mpsc;
+
+pub type UiSender = std::sync::mpsc::Sender<Box<Fn(&mut Cursive) + Send>>;
+
+pub type LoginDataSender = Arc<Mutex<Option<oneshot::Sender<(Ticket, String)>>>>;
+
+pub fn login_dialog(result: LoginDataSender) -> views::Dialog {
     use cursive::view::{Boxable, Identifiable};
     let username = views::LinearLayout::horizontal()
         .child(views::TextView::new("Username"))
@@ -15,26 +26,34 @@ fn login_dialog() -> views::Dialog {
         .child(username)
         .child(password);
     views::Dialog::around(inputs)
-        .button("Login", |siv| select_character(siv))
+        .button("Login", move |siv| select_character(siv, result.clone()))
         .button("Quit", |siv| siv.quit())
 }
 
-fn select_character(siv: &mut Cursive) {
+fn select_character(siv: &mut Cursive, result: LoginDataSender) {
     let username = siv.call_on_id("username", |text: &mut views::EditView| text.get_content()).expect("Failed to find ID \"username\"");
     let password = siv.call_on_id("password", |text: &mut views::EditView| text.get_content()).expect("Failed to find ID \"password\"");
     let ticket = Ticket::request(&username, &password).unwrap();
     siv.pop_layer();
     let mut characters = views::SelectView::new();
     characters.add_all_str(ticket.characters().iter().cloned());
-    characters.set_on_submit::<_, str>(|siv, character| {
-        siv.pop_layer();
-    });
+    characters.set_on_submit::<_, str>(
+        move |siv, character| {
+            let result = result.lock().unwrap().take().expect("select_character result is None");
+            result.send((ticket.clone(), String::from(character))).unwrap();
+            siv.pop_layer();
+        }
+    );
     siv.add_layer(characters);
 }
 
-pub fn start() {
-    let mut siv = cursive::Cursive::new();
-    siv.set_fps(30);
-    siv.add_layer(login_dialog());
-    siv.run();
+pub fn start() -> UiSender {
+    let (tx, rx) = mpsc::channel();
+    spawn(move || {
+        let mut siv = cursive::Cursive::new();
+        tx.send(siv.cb_sink().clone());
+        siv.set_fps(30);
+        siv.run();
+    });
+    rx.recv().unwrap()
 }
